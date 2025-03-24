40b29e5312c3ad2f533f5427ae4774bc2f3fb1f2
Aditya1or0 <adityapandit264@gmail.com>
2025-03-24 15:34:53 +0530

    added bottom Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
-import { Button } from './components/ui/button'
+import React from 'react';
 
-const App : React.FC= () => {
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import BottomNavbar from './components/Bottom';
+
+const App: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <h1>hello</h1>
-    <Button>Click me</Button>
-  </div>
-  )
-}
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        <Routes>
+          <Route path="/" element={<div className="flex-1">Home Page</div>} />
+          <Route path="/search" element={<div className="flex-1">Search Page</div>} />
+          <Route path="/profile" element={<div className="flex-1">Profile Page</div>} />
+        </Routes>
+        
+        <BottomNavbar />
+      </div>
+    </Router>
+  );
+};
 
-export default App
+export default App;
