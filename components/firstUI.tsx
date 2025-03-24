20974d1977e12c627a627ae6e55d0218b135c6dc
shubhlodhi <shubh.singh9411@gmail.com>
2025-03-24 15:37:41 +0530

    Added new feature XYZ

---
+++ components/firstUI.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FirstUI: React.FC = () => {
+  return (
+    <>
+      <h1 className="text-2xl font-bold text-center text-blue-600">
+        Welcome to FirstUI Component!
+      </h1>
+    </>
+  );
+};
+
+export default FirstUI;
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Button } from './components/ui/button'
+import FirstUI from "../components/firstUI"
 
 const App : React.FC= () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
       <h1>hello</h1>
     <Button>Click me</Button>
+    <FirstUI/>
   </div>
   )
 }
