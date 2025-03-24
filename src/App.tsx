392d517e879bcfcda92dcccc90616ea0223ebd2e
bipinjoshi <bipinjoshicr712@gmail.com>
2025-03-24 15:23:56 +0530

    bipi first

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Button } from './components/ui/button'
 
-const App : React.FC= () => {
+const App: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
-      <h1>hello</h1>
-    <Button>Click me</Button>
-  </div>
+      <h1>hell</h1>
+      <Button>Click me</Button>
+    </div>
   )
 }
 
